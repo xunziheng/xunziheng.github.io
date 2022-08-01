45a2e681f6d282e0de7071d21892f711dd60320c
xunziheng <xunziheng88@163.com>
2022-08-01 14:39:24 +0800

    remove demo

--- 2022/src/index.js
+++ 2022/src/index.js
@@ -1,37 +1,27 @@
 import Phaser from 'phaser';
 import './css/index.css'
-import logoImg from './assets/logo.png';
 
-class MyGame extends Phaser.Scene
+class MainGame extends Phaser.Scene
 {
   constructor () {
     super();
   }
 
   preload () {
-    this.load.image('logo', logoImg);
+
   }
     
   create () {
-    const logo = this.add.image(400, 150, 'logo');
-  
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1
-    });
+    
   }
 }
 
 const config = {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
+  parent: 'phaser-root',
   width: window.innerWidth,
   height: window.innerHeight,
-  scene: MyGame
+  scene: MainGame
 };
 
 const game = new Phaser.Game(config);
