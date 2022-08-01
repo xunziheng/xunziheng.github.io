ae2da3f836c49c495ca3ff352269e9e27f69659c
xunziheng <xunziheng88@163.com>
2022-08-01 11:31:42 +0800

    full screen

--- 2022/src/index.js
+++ 2022/src/index.js
@@ -1,39 +1,41 @@
 import Phaser from 'phaser';
+import './css/index.css'
 import logoImg from './assets/logo.png';
 
 class MyGame extends Phaser.Scene
 {
-    constructor ()
-    {
-        super();
-    }
+  constructor () {
+    super();
+  }
 
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+  preload () {
+    this.load.image('logo', logoImg);
+  }
+    
+  create () {
+    const logo = this.add.image(400, 150, 'logo');
+  
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scene: MyGame
 };
 
 const game = new Phaser.Game(config);
+
+window.addEventListener('resize', function (event) {
+  game.scale.resize(window.innerWidth, window.innerHeight);
+}, false);
